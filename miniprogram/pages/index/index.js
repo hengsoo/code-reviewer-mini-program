3eea5abf531e82c0221da19967564cd85a3f6551
hengsoo.sjh@gmail.com <hengsoo.sjh@gmail.com>
2020-04-22 20:31:54 +0800

    Add menu list

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -7,46 +7,47 @@ Page({
   },
 
   onLoad: function() {
-    setOpenID()
-    getUserMenu()
+    this.setOpenID()
+    this.getUserMenu()
+  },
+  
+  setOpenID: function() {
+    // Call cloud funtion getOpenID
+    wx.cloud.callFunction({
+      name: 'getOpenID',
+      data: {},
+      success: res => {
+        console.log('user openid: ', res.result.openid)
+        app.globalData.openid = res.result.openid
+      },
+      fail: err => {
+        console.error('[CLOUD] [getOpenID] FAILED: ', err)
+      }
+    })
   },
 
-})
-
-function setOpenID() {
-  // Call cloud funtion getOpenID
-  wx.cloud.callFunction({
-    name: 'getOpenID',
-    data: {},
-    success: res => {
-      console.log('[CLOUD] [getOpenID] user openid: ', res.result.openid)
-      app.globalData.openid = res.result.openid
-    },
-    fail: err => {
-      console.error('[CLOUD] [getOpenID] FAILED: ', err)
-    }
-  })
-}
-
-function getUserMenu(){
-  // Call cloud funtion getOpenID
-  wx.cloud.callFunction({
-    name: 'getUserMenu',
-    data: {},
-    success: res => {
-      console.log(res)
-      updateUserMenu(res.result.menu)
-    },
-    fail: err => {
-      console.log('Get user menu FAILED: ', err)
-    }
-  })
-}
-
-function updateUserMenu(new_menu){
-  if (new_menu){
-    this.setData({
-      menu :  new_menu
+  getUserMenu: function(){
+    // Call cloud funtion getOpenID
+    wx.cloud.callFunction({
+      name: 'getUserMenu',
+      data: {},
+      success: res => {
+        this.updateUserMenu(res.result.data.menu)
+        console.log("user menu updated")
+      },
+      fail: error => {
+        console.log('Get user menu FAILED: ', error)
+      }
     })
+  },
+
+  updateUserMenu: function(new_menu){
+    console.log(new_menu)
+    if (new_menu){
+      this.setData({
+        menu :  new_menu
+      })
+    }
   }
-}
+  
+})
