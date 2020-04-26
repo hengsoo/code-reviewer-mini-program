d19ce80af4dc211971d0c53e0622f1b18d748eee
hengsoo.sjh@gmail.com <hengsoo.sjh@gmail.com>
2020-04-26 20:57:53 +0800

    Remove unnecessary comment

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -98,16 +98,6 @@ Page({
         console.error("Choose file failed:", error)
       }
     })
-  },
-  /*displayCode: function(e){
-    // var model = JSON.stringify(data.menu[0].file_id);
-    var id = e.currentTarget.dataset.file_name;
-    console.log(this.data.menu[0].file_id);
-    console.log(e.target.dataset);
-    console.log(e.currentTarget.dataset);
-    wx.navigateTo({
-      url: '../code-view/code-view?file_id={{this.data.menu[id].file_id}}',
-      // todo 传入文件序号，在另一端读取文件序号，再从数据库中得到code
-    })
-  }*/
+  }
+
 })
