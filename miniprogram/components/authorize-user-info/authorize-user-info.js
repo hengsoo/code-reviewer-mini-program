c24b05a9c5675b893630e754244ecff8b65757a3
hengsoo.sjh@gmail.com <hengsoo.sjh@gmail.com>
2020-04-29 01:04:26 +0800

    Add header to each page. Header includes authorize-user-info component

--- miniprogram/components/authorize-user-info/authorize-user-info.js
+++ miniprogram/components/authorize-user-info/authorize-user-info.js
@@ -5,46 +5,42 @@ Component({
     addGlobalClass: true
   },
 
+  data: {
+    show_dialog: false
+  },
+
   lifetimes: {
     attached: function() {
-      console.log("banana")
-      if (!app.globalData.user_info){
-        this.setData({
-          show: true
-        })
-      }
-    },
+      console.log("BABABA")
+      this.getUserInfo()
+    }
   },
   
   methods: {
-    close(e) {
-      const { type } = e.currentTarget.dataset;
+    getUserInfo: function(){
+      wx.getUserInfo({
+        success: res => {
+          console.log("getUserInfo success")
+          app.globalData.user_info = res.userInfo
+        },
+        fail: error =>{
+          console.error("getUserInfo failed: ", error)
+          this.setData({
+            show_dialog: true
+          })
+        }
+      })
+    },
 
+    close(event) {
+      const { type } = event.currentTarget.dataset;
       if ( type === 'close') {
         this.setData({
-          show: false
-        }); // 关闭弹窗回调事件
-        
-        // Custom authorize getUserInfo Logic       
-        // const app = getApp()
-        // app.globalData.request_authorize_user_info_dialog = false
-        this.triggerEvent('close');
-
-        wx.getUserInfo({
-          success: res => {
-              console.log(res)
-              console.log('user name: ', res)
-              // this.data.userInfo = res.userInfo;
-              // app.globalData.username = res.result.openid
-          },
-          fail: err => {
-            console.error('[CLOUD] [getUserInfo] FAILED: ', err)
-            this.setData({
-              show: true
-            })
-          }
-        })
+          show_dialog: false
+        });
+        this.getUserInfo()
       }
-   }
+    }
   }
+  
 })
