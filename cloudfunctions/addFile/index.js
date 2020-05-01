3f6010d155510e2f633a3d1c7cfc62e2dbc01696
hengsoo.sjh@gmail.com <hengsoo.sjh@gmail.com>
2020-05-01 20:05:44 +0800

    Export syntaxHighlight function, refactor addFile function

--- cloudfunctions/addFile/index.js
+++ cloudfunctions/addFile/index.js
@@ -4,107 +4,81 @@ cloud.init({
   env: cloud.DYNAMIC_CURRENT_ENV
 })
 
+const db = cloud.database()
+
 exports.main = async (event, context) => {
 
   const user_openid = cloud.getWXContext().OPENID
-  const db = cloud.database()
-  const prism = require('./utils/prism.js')
+
   const file_name = event.file_name
-  const raw_file_content = event.file_content.split("\n")
-  const file_extension = file_name.split(".").pop()
-  const [language, grammar] = getLanguageAndGrammar(file_extension, prism)
-  const file_id = file_name + "_" + Date.now() +"_"+ user_openid
+  const raw_file_content = event.file_content
+  const file_id = file_name + "_" + Date.now() + "_" + user_openid
 
-  console.log("Add new file:")
-  console.log(file_id + " " + language)
+  console.log("Add new file: ", file_id)
 
-  let file_highlighted_content = []
-
-  raw_file_content.forEach( line => {
-    if (line === ""){
-      file_highlighted_content.push(" ")
-    }
-    else{
-      // Highlight line
-      line = prism.highlight(line, grammar, language)
-      // Repalce 4 spaces and tab with nbsp
-      line = line.replace(/    |	/g,"&nbsp;&nbsp;&nbsp;&nbsp;")
-      // Push line
-      file_highlighted_content.push(line)
-    }
-  })
-
-  console.log("Add entry")
-  await db.collection('program-files').add({
-    data:{
-      _id : file_id,
-      _openid : user_openid,
-      created_at: new Date(),
-      language: language,
-      code: file_highlighted_content,
-      reviews: []
-    }
-  })
-
-  console.log("Update user menu")
-  await db.collection('user-menus')
-  .where({
-    _openid: user_openid
-  })
-  .update({
-    data: {
-      menu: db.command.push(
-        {
-        file_id: file_id,
+  try {
+    let {
+      highlighted_content,
+      language
+    } = (await cloud.callFunction({
+      name: 'syntaxHighlight',
+      data: {
         file_name: file_name,
-        language: language
-      })
-    }
-  })
+        file_content: raw_file_content
+      }
+    })).result
 
-  const menu_id = `program_file_menu_` + user_openid
-  const updated_menu = await db.collection('user-menus').doc(menu_id).get()
+    addEntry(user_openid, file_id, language, highlighted_content)
+    updateUserMenu(user_openid, file_id, file_name, language)
 
-  return updated_menu
-}
+    const menu_id = `program_file_menu_` + user_openid
+    const updated_menu = await db.collection('user-menus').doc(menu_id).get()
 
-function getLanguageAndGrammar(file_extension, prism){
-  let language = ""
-  let grammar = {}
-  switch(file_extension){
-    case "c":
-      language = "c"
-      grammar = prism.languages.c
-      break
-    case "cpp":
-      language = "cpp"
-      grammar = prism.languages.cpp
-      break
-    case "py":
-      language = "python"
-      grammar = prism.languages.python
-      break
-    case "js":
-      language = "javascript"
-      grammar = prism.languages.javascript
-      break
-    case "java":
-      language = "java"
-      grammar = prism.languages.java
-      break
-    case "html":
-      langauage = "markup"
-      grammar = prism.languages.markup
-      break
-    case "JSON":
-    case "json":
-      language = "json"
-      grammar = prism.languages.json
-      break
-    default:
-      langauge = "clike"
-      grammar = prism.languages.clike
+    return updated_menu
+  }
+  // If syntaxHighlight failed
+  catch (error) {
+    console.error('syntaxHighlight FAILED: ', error)
+    return null
   }
 
-  return [language, grammar]
+}
+
+async function addEntry(user_openid, file_id, language, file_highlighted_content) {
+  console.log("Add entry")
+  try {
+    await db.collection('program-files').add({
+      data: {
+        _id: file_id,
+        _openid: user_openid,
+        created_at: new Date(),
+        language: language,
+        code: file_highlighted_content,
+        reviews: []
+      }
+    })
+  } catch (error) {
+    console.error("Add entry failed: ", error)
+  }
+}
+
+async function updateUserMenu(user_openid, file_id, file_name, language) {
+  console.log("Update user menu")
+  try {
+    await db.collection('user-menus')
+      .where({
+        _openid: user_openid
+      })
+      .update({
+        data: {
+          menu: db.command.push({
+            file_id: file_id,
+            file_name: file_name,
+            language: language
+          })
+        }
+      })
+  } catch (error) {
+    console.error("Update user menu failed: ", error)
+  }
 }
