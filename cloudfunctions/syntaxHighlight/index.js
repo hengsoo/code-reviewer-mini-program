fc6c4f9cc08c89db3a1829f212807db72cd1cc18
HarryWu-CHN <904714159@qq.com>
2020-05-04 11:14:09 +0800

    Add more language types

--- cloudfunctions/syntaxHighlight/index.js
+++ cloudfunctions/syntaxHighlight/index.js
@@ -69,6 +69,85 @@ function getLanguageAndGrammar(file_extension, prism){
       language = "json"
       grammar = prism.languages.json
       break
+    case "cs":
+      language = "csharp"
+      grammar = prism.langauages.csharp
+      break
+    case "go":
+      language = "go"
+      grammar = prism.langauages.go
+      break
+    case "css":
+      language = "css"
+      grammar = prism.langauages.css
+      break
+    case "md":
+      language = "markdown"
+      grammar = prism.langauages.markdown
+      break
+    case "dtl":
+      language = "django"
+      grammar = prism.langauages.django
+      break
+    case "php":
+    case "php4":
+    case "php5":
+      language = "php"
+      grammar = prism.langauages.php
+      break
+    case "cl":
+      language = "opencl"
+      grammar = prism.langauages.opencl
+      break
+    case "m":
+      language = "matlab"
+      grammar = prism.langauages.matlab
+      break
+    case "tex":
+    case "latex":
+      language = "latex"
+      grammar = prism.langauages.latex
+      break
+    case "kt":
+      language = "kotlin"
+      grammar = prism.langauages.kotlin
+      break
+    case "r":
+      language = "r"
+      grammar = prism.langauages.r
+      break
+    case "jsx":
+      language = "jsx"
+      grammar = prism.langauages.jsx
+      break
+    case "rb":
+      language = "ruby"
+      grammar = prism.langauages.ruby
+      break
+    case "rs":
+      language = "rust"
+      grammar = prism.langauages.rust
+      break
+    case "scss":
+      language = "sass"
+      grammar = prism.langauages.sass
+      break
+    case "sql":
+      language = "sql"
+      grammar = prism.langauages.sql
+      break
+    case "swift":
+      language = "swift"
+      grammar = prism.langauages.swift
+      break
+    case "v":
+      language = "verilog"
+      grammar = prism.langauages.verilog
+      break
+    case "vhd":
+      language = "vhdl"
+      grammar = prism.langauages.vhdl
+      break
     default:
       language = "clike"
       grammar = prism.languages.clike
