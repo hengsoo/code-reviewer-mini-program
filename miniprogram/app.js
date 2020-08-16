ff3dc00adf1534ed6c7c7794db7fa4f7074e45c0
hengsoo <hengsoo.sjh@gmail.com>
2020-08-16 16:43:49 +0800

    Add comment

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -6,7 +6,7 @@ App({
       console.error('请使用 2.2.3 或以上的基础库以使用云能力')
     } else {
       wx.cloud.init({
-        // env: 'code-reviewer',
+        // DEVELOPMENT ENV
         env: 'cloud-test-18vr2',
         traceUser: true,
       })
