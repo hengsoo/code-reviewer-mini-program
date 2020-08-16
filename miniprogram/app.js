2ff48afa447809944293de202addd7ad923dff74
hengsoo <hengsoo.sjh@gmail.com>
2020-08-16 16:44:41 +0800

    Update env to production

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -6,8 +6,8 @@ App({
       console.error('请使用 2.2.3 或以上的基础库以使用云能力')
     } else {
       wx.cloud.init({
-        // env: 'code-reviewer',
-        env: 'cloud-test-18vr2',
+        // PRODUCTION ENV
+        env: 'code-reviewer',
         traceUser: true,
       })
     }
